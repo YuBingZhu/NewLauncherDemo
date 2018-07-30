f796aac1ad2f50aa8b09503c9cb7ffdb418b28d8
bingpan <cn-bing.pan@ff.com>
2018-07-30 13:27:00 +0800

    test notification channel.
    Signed-off-by: bingpan <cn-bing.pan@ff.com>

--- app/src/main/java/com/launcher/demo/addview/AddViewActivity.java
+++ app/src/main/java/com/launcher/demo/addview/AddViewActivity.java
@@ -1,14 +1,23 @@
 package com.launcher.demo.addview;
 
 import android.app.Activity;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.BitmapFactory;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.launcher.demo.MainActivity;
 import com.launcher.demo.R;
 
 public class AddViewActivity extends Activity {
@@ -40,7 +49,32 @@ public class AddViewActivity extends Activity {
             container.addView(textView);
         }
 
+        createNotificationChannel();
+        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this);
+        mBuilder.setSmallIcon(R.mipmap.ic_launcher);
+        mBuilder.setLargeIcon(BitmapFactory.decodeResource(this.getResources(), R.mipmap.ic_launcher));
+        mBuilder.setContentTitle("这是标题");
+        mBuilder.setContentText("这是正文，当前ID是：" + 1);
+        mBuilder.setSubText("这是摘要");
+        mBuilder.setAutoCancel(true);
+        mBuilder.setContentInfo("Info");
+        mBuilder.setNumber(2);
+        mBuilder.setTicker("在状态栏上显示的文本");
+        mBuilder.setPriority(NotificationCompat.PRIORITY_MAX);
+        mBuilder.setWhen(System.currentTimeMillis() - 3600000);
+        mBuilder.setOngoing(true);
+        mBuilder.setDefaults(NotificationCompat.DEFAULT_SOUND);
+        mBuilder.setVibrate(new long[]{0, 1000, 1000, 1000});
+        mBuilder.setChannelId("newDemo");
+        Intent intent = new Intent(this, MainActivity.class);
+        PendingIntent pIntent = PendingIntent.getActivity(this, 0, intent, 0);
+        mBuilder.setContentIntent(pIntent);
+        NotificationManager mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        mNotificationManager.notify(1, mBuilder.build());
+
     }
+
+    private int clickCount;
 
     public void add_view(View view) {
         view.setOnClickListener(new View.OnClickListener() {
@@ -48,6 +82,7 @@ public class AddViewActivity extends Activity {
             public void onClick(View v) {
                 showToast("click add view button.");
 
+                clickCount++;
                 TextView textView = new TextView(AddViewActivity.this);
                 LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
                         LinearLayout.LayoutParams.WRAP_CONTENT,
@@ -57,7 +92,7 @@ public class AddViewActivity extends Activity {
                 textView.setTextSize(30);
                 textView.setTextColor(getResources().getColor(R.color.colorPrimary));
                 textView.setBackgroundColor(getResources().getColor(R.color.colorAccent));
-                textView.setText(" new added view !!!");
+                textView.setText(" new added view:" + clickCount);
                 container.addView(textView, 3);
             }
         });
@@ -70,5 +105,21 @@ public class AddViewActivity extends Activity {
         }
         toast = Toast.makeText(this, text, Toast.LENGTH_SHORT);
         toast.show();
+    }
+
+    private void createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = "channel name";
+            String description = "channel_description";
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel("newDemo", name, importance);
+            channel.setDescription(description);
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
     }
 }
